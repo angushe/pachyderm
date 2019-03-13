30850a38ab935abb5d6529d40598e9a922ed1ea8
Adele Lopez <adele@pachyderm.io>
2019-03-12 17:19:36 -0700

    use CommitOrigin for provenance everywhere

--- src/server/pachyderm_test.go
+++ src/server/pachyderm_test.go
@@ -8450,7 +8450,7 @@ func TestSpout(t *testing.T) {
 			commitInfo, err := iter.Next()
 			require.NoError(t, err)
 			require.Equal(t, 1, len(commitInfo.Provenance))
-			provenance := commitInfo.Provenance[0]
+			provenance := commitInfo.Provenance[0].Commit
 			if i == 0 {
 				// set first one
 				provenanceID = provenance.ID
@@ -8487,7 +8487,7 @@ func TestSpout(t *testing.T) {
 			commitInfo, err := iter.Next()
 			require.NoError(t, err)
 			require.Equal(t, 1, len(commitInfo.Provenance))
-			provenance := commitInfo.Provenance[0]
+			provenance := commitInfo.Provenance[0].Commit
 			if i == 0 {
 				// this time, we expect our commits to have different provenance from the commits earlier
 				require.NotEqual(t, provenanceID, provenance.ID)
